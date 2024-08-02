9ccc488b3cc1637dfce225d0b0f20fad2fb44a22
Akankshamarko <akankshasinghmarko123@gmail.com>
2024-08-02 20:54:08 +0530

    2nd day of java

---
+++ string.java
@@ -0,0 +1,12 @@
+public class string {
+    public static void main(String[] args) {
+        char  ch ='s';
+
+        String s = String.valueOf(ch);
+        System.out.println("value of string is " +s);
+
+
+
+    }
+    
+}
