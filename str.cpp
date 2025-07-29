e7e7757b24d612a274d5a03ff7f3e0ecc79168ed
akanksha singh <akankshasinghmarko123@gmail.com>
2025-07-29 22:04:17 +0530

    Add files via upload

---
+++ str.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str = "Hello, World!";//dyanimc in nature
+    cout<<str<<endl;
+    str = "Hello, C++!";
+    cout<<str<<endl;
+    return 0;
+    
+}
